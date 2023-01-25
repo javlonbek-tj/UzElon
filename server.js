2f596f34d7a2c754af506552e626b8b015f16fb4
tjdeveloper <tjmr1996@gmail.com>
2023-01-25 14:02:56 +0500

    new packages added

---
+++ server.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  process.exit(1);
+});
+dotenv.config();
+const app = require('./app');
+
+const DB = process.env.MONGO_URL;
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection succesful');
+});
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`App running on ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
