a35de2783d28c92d1d64effd0b1ed82e041e1be4
tjdeveloper <tjmr1996@gmail.com>
2023-05-05 07:18:21 +0500

    secure for https

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,6 +12,7 @@ const createSendToken = (user, req, res) => {
   res.cookie('jwt', token, {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
   });
 };
 
