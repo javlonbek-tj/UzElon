b90d7f615cabc4af9e53f0ac44822ec3f978c84f
tjdeveloper <tjmr1996@gmail.com>
2023-06-05 21:07:12 +0500

    auth admin

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 const { postMakeProductTop, postMakeProductBottom } = require('../controllers/admin');
+const { restrictTo } = require('../controllers/auth');
+const isAuth = require('../middleware/is-auth');
 
-router.post('/makeProductTop', postMakeProductTop);
-router.post('/makeProductBottom', postMakeProductBottom);
+router.post('/makeProductTop', isAuth, restrictTo('admin'), postMakeProductTop);
+router.post('/makeProductBottom', isAuth, restrictTo('admin'), postMakeProductBottom);
 
 module.exports = router;
