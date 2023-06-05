3db911baa6958983f52a1e12a3686597a152910e
tjdeveloper <tjmr1996@gmail.com>
2023-06-05 21:40:24 +0500

    expiresIn changed to seconds

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ const AppError = require('../utils/appError');
 
 const createSendToken = (user, req, res) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    expiresIn: parseInt(process.env.JWT_EXPIRES_IN),
   });
 
   res.cookie('jwt', token, {
